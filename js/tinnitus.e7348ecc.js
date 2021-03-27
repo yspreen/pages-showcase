2ca675a4fd1b728c8afe7af6d3b7482d7d3cd812
Yannick Spreen <yannick.spreen@gmail.com>
2021-03-28 00:52:50 +0100

    Add temporary website.

---
+++ js/tinnitus.e7348ecc.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["tinnitus"],{"10ec":function(n,t,e){"use strict";e.r(t);var c=e("7a23"),i={class:"about"},s=Object(c["h"])("h1",null,"Tinnitus",-1);function u(n,t){return Object(c["q"])(),Object(c["e"])("div",i,[s])}const o={};o.render=u;t["default"]=o}}]);
+//# sourceMappingURL=tinnitus.e7348ecc.js.map
